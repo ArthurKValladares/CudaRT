05f8b5701cd33173f39d06fd2ba9eae628009dd7
ArthurValladares <arthurkaukal@gmail.com>
2024-04-27 20:39:41 -0700

    start refactor to write to surface, not working yet

--- CudaRT/kernel.cu
+++ CudaRT/kernel.cu
@@ -37,11 +37,43 @@ __global__ void render(vec3* fb, int max_x, int max_y, vec3 lower_left_corner, v
     fb[pixel_index] = color(r);
 }
 
+__device__ Uint32 sdl_color(const ray& r) {
+    const vec3 unit_direction = unit_vector(r.direction());
+    const float t = 0.5f * (unit_direction.y() + 1.0f);
+
+    const Uint8 R = lerp(1.0, 0.5, t) * 255.;
+    const Uint8 G = lerp(1.0, 0.7, t) * 255.;
+    const Uint8 B = lerp(1.0, 1.0, t) * 255.;
+
+    return (0xFF << 24) | (R << 16) | (G << 8) | (B);
+}
+
+__global__ void sdl_render(Uint32* fb, int max_x, int max_y, vec3 lower_left_corner, vec3 horizontal, vec3 vertical, vec3 origin) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if ((i >= max_x) || (j >= max_y)) return;
+    int pixel_index = j * max_x + i;
+    float u = float(i) / float(max_x);
+    float v = float(j) / float(max_y);
+    ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+    fb[pixel_index] = sdl_color(r);
+}
+
 int main() {
+    clock_t start, stop;
+
     int nx = 1200;
     int ny = 600;
     int tx = 8;
     int ty = 8;
+
+    int num_pixels = nx * ny;
+    size_t fb_size = num_pixels * sizeof(vec3);
+
+    const size_t surface_buffer_size = nx * ny * sizeof(Uint32);
+
+    dim3 blocks(nx / tx + 1, ny / ty + 1);
+    dim3 threads(tx, ty);
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not be initialized!\n"
@@ -49,7 +81,6 @@ int main() {
             SDL_GetError());
         return 0;
     }
-
     SDL_Window* window = SDL_CreateWindow(
         "Basic C SDL project", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
         nx, ny, SDL_WINDOW_SHOWN);
@@ -72,54 +103,7 @@ int main() {
         return 0;
     }
     Uint32* surface_buffer;
-    checkCudaErrors(cudaMalloc((void**)&surface_buffer, nx * ny * sizeof(Uint32)));
-
-    std::cerr << "Rendering a " << nx << "x" << ny << " image ";
-    std::cerr << "in " << tx << "x" << ty << " blocks.\n";
-
-    int num_pixels = nx * ny;
-    size_t fb_size = num_pixels * sizeof(vec3);
-
-    // allocate FB
-    vec3* fb;
-    checkCudaErrors(cudaMallocManaged((void**)&fb, fb_size));
-
-    clock_t start, stop;
-    start = clock();
-    // Render our buffer
-    dim3 blocks(nx / tx + 1, ny / ty + 1);
-    dim3 threads(tx, ty);
-    render << <blocks, threads >> > (
-        fb, nx, ny,
-        vec3(-2.0, -1.0, -1.0),
-        vec3(4.0, 0.0, 0.0),
-        vec3(0.0, 2.0, 0.0),
-        vec3(0.0, 0.0, 0.0)
-    );
-    checkCudaErrors(cudaGetLastError());
-    checkCudaErrors(cudaDeviceSynchronize());
-
-    // Output FB as Image
-    std::ofstream output_image;
-    //txtOut.open ("Example.txt");
-    output_image.open("output.ppm");
-    output_image << "P3\n" << nx << " " << ny << "\n255\n";
-    for (int j = ny - 1; j >= 0; j--) {
-        for (int i = 0; i < nx; i++) {
-            size_t pixel_index = j * nx + i;
-            int ir = int(255.99 * fb[pixel_index].r());
-            int ig = int(255.99 * fb[pixel_index].g());
-            int ib = int(255.99 * fb[pixel_index].b());
-            output_image << ir << " " << ig << " " << ib << "\n";
-        }
-    }
-    output_image.close();
-
-    stop = clock();
-    double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
-    std::cerr << "took " << timer_seconds << " seconds.\n";
-
-    checkCudaErrors(cudaFree(fb));
+    checkCudaErrors(cudaMalloc((void**)&surface_buffer, surface_buffer_size));
 
     bool quit = false;
     while (!quit) {
@@ -149,9 +133,24 @@ int main() {
         }
 
         SDL_LockSurface(surface);
+        {
+            start = clock();
 
-        // TODO: Render
+            sdl_render << <blocks, threads >> > (
+                surface_buffer, nx, ny,
+                vec3(-2.0, -1.0, -1.0),
+                vec3(4.0, 0.0, 0.0),
+                vec3(0.0, 2.0, 0.0),
+                vec3(0.0, 0.0, 0.0)
+                );
+            checkCudaErrors(cudaGetLastError());
+            checkCudaErrors(cudaDeviceSynchronize());
+            checkCudaErrors(cudaMemcpy(surface->pixels, surface_buffer, surface_buffer_size, cudaMemcpyDeviceToHost));
 
+            stop = clock();
+            double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+            std::cerr << "took " << timer_seconds << " seconds.\n";
+        }
         SDL_UnlockSurface(surface);
         SDL_UpdateWindowSurface(window);
     }
