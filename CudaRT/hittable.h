2deb738fb6ad57b4df611ab1fe3dabb4d970f790
ArthurValladares <arthurkaukal@gmail.com>
2024-05-12 23:06:15 -0700

    set front face in hit_record

--- CudaRT/hittable.h
+++ CudaRT/hittable.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <assert.h>
+
+#include "math.h"
 #include "vec3.h"
 #include "ray.h"
 
@@ -8,4 +11,12 @@ public:
 	vec3 p;
 	vec3 normal;
 	float t;
+	bool front_face;
+
+	__device__ void set_face_normal(const ray& r, const vec3& outward_normal) {
+		assert(float_equals(outward_normal.length(), 1.0));
+
+		front_face = dot(r.direction(), outward_normal) < 0;
+		normal = front_face ? outward_normal : -outward_normal;
+	}
 };
--- CudaRT/math.h
+++ CudaRT/math.h
@@ -1,6 +1,13 @@
 #pragma once
 
+#define EPSILON 0.000001f
+
 template<class T>
 __host__ __device__ T lerp(T a, T b, float t) {
 	(1.0 - t) * a + t * b;
+}
+
+__host__ __device__ bool float_equals(float x, float y)
+{
+    return (fabsf(x - y)) <= (EPSILON * fmaxf(1.0f, fmaxf(fabsf(x), fabsf(y))));
 }
