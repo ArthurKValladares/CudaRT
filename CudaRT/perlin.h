dea11aa41e8baa68a07a4e17e17958c47fa80009
ArthurKValladares <arthurkaukal@gmail.com>
2024-07-17 18:48:22 -0400

    trilinear interpolation smoothing

--- CudaRT/perlin.h
+++ CudaRT/perlin.h
@@ -17,11 +17,29 @@ struct Perlin {
     }
 
     __device__ float noise(const Vec3f32& p) const {
-        auto i = int(4 * p.x()) & 255;
-        auto j = int(4 * p.y()) & 255;
-        auto k = int(4 * p.z()) & 255;
+        const float u = p.x() - floor(p.x());
+        const float v = p.y() - floor(p.y());
+        const float w = p.z() - floor(p.z());
 
-        return randfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+        const int i = int(floor(p.x()));
+        const int j = int(floor(p.y()));
+        const int k = int(floor(p.z()));
+
+        float c[2][2][2];
+
+        for (int di = 0; di < 2; di++) {
+            for (int dj = 0; dj < 2; dj++) {
+                for (int dk = 0; dk < 2; dk++) {
+                    c[di][dj][dk] = randfloat[
+                        perm_x[(i + di) & 255] ^
+                        perm_y[(j + dj) & 255] ^
+                        perm_z[(k + dk) & 255]
+                    ];
+                }
+            }
+        }
+
+        return trilinear_interp(c, u, v, w);
     }
 
     static const int point_count = 256;
@@ -48,5 +66,21 @@ private:
             p[i] = p[target];
             p[target] = tmp;
         }
+    }
+
+    __device__ static float trilinear_interp(float c[2][2][2], float u, double v, float w) {
+        auto accum = 0.0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    accum += (i * u + (1 - i) * (1 - u))
+                        * (j * v + (1 - j) * (1 - v))
+                        * (k * w + (1 - k) * (1 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+
+        return accum;
     }
 };
